75f2a10d49a89cd6ef19547b8e63547c38f7510c
Joey V <joey03v@gmail.com>
2024-03-06 21:36:50 -0600

    finished querys just need to do mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,10 +2,30 @@ const {User,Pet,Post,Reaction} = require('../models')
 
 const resolvers = {
     Query: {
-
+        posts: async () => {
+            return await Post.find({})
+        },
+        users: async () => {
+            return await User.find({}).populate('posts').populate({
+                path: 'posts',
+                populate: 'reactions'
+            }).populate('pets').populate('friends')
+        },
+        reactions: async () => {
+            return await Post.find({}).populate('reactions')
+        },
+        pets: async () => {
+            return await User.find({}).populate('pets')
+        },
+        friends: async () => {
+            return await User.find({}).populate('friends')
+        }
     },
     Mutation: {
-
+        createPost: async (parent,args) => {
+            const post = await Post.create(args)
+            return post
+        }
     }
 }
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,38 @@
 const typeDefs = `
+type Post {
+    username: String
+    createdAt: String
+    postImage: Image
+    post Text: String
+    reactions: [Reactions]
+}
 
+type Reaction {
+    username: String
+    reactionBody: String
+    createdAt: String
+}
 
+Type User {
+    username: String
+    posts: [Post]
+    pets: [pets]
+    friends: [User]
+}
 
+Type Pet {
+    name: String
+    breed: String
+    gender: String
+}
+
+type Query {
+    posts: [Post]
+    users: [User]
+    reactions: [Reactions]
+    pets: [Pet]
+    friends: [User]
+}
 `
 
 module.exports = typeDefs
